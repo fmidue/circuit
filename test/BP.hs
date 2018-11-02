7cf0acc5aa49b1c0ec8b90c22d3f465390ffe83b
Johannes Waldmann <johannes.waldmann@htwk-leipzig.de>
2018-11-02 17:09:14 +0100

    initial version of documentation

--- test/BP.hs
+++ test/BP.hs
@@ -4,4 +4,7 @@ import Graphics.Gnuplot.Simple
 import Circuit
 
 -- | impulse response for band pass filter
-main = plotList [ EPS "data/BP.eps" ] $ take 1000 $  apply_func bp 0.02 $ impulse 0.1 1
+main = plotList [ EPS "data/BP.eps" ] 
+     $ take 1000 
+     $ apply_func bp (Second 0.02)
+     $ impulse (Second 0.1) (Volt 1)
